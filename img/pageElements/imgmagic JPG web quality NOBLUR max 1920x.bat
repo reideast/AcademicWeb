fbd7eacc4ec20f0f4f1c2ae2991b2a5d142f7739
andrew@andreweast.net <andrew@andreweast.net>
2016-02-26 19:52:25 +0000

    All attempts at smooth parallax scrolling with javascript have failed. Creating a new branch now...

---
+++ img/pageElements/imgmagic JPG web quality NOBLUR max 1920x.bat
@@ -0,0 +1,3 @@
+convert -resize 1920x^> -strip -interlace Plane -sampling-factor 4:2:0 -quality 85%% %1 "ImageMagic\%~nx1"
+
+pause
---
+++ img/pageElements/imgmagic JPG web quality blur max 1920x.bat
@@ -0,0 +1,3 @@
+convert -resize 1920x^> -strip -interlace Plane -gaussian-blur 0.05 -quality 85%% %1 "ImageMagic\%~nx1"
+
+pause
