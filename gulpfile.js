17ab4acb2cd3a4029762c53c265e19201955d19a
Andrew Reid East <andrew@andreweast.net>
2019-03-13 21:39:49 +0000

    Fix gulp for new versions

--- gulpfile.js
+++ gulpfile.js
@@ -3,21 +3,23 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var cleanCSS = require('gulp-clean-css');
+sass.compiler = require('node-sass');
 
 gulp.task('sass', function() {
-    gulp.src('sass/*.scss')
-    .pipe(sass())
-    .on('error', function(err) {
-      console.log(err); 
-      this.emit('end');
-    })
-    .pipe(autoprefixer())
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('css'))
+    return gulp.src('sass/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest('css')) // './css' orginally
 });
 
 gulp.task('default', function() {
-    gulp.watch('sass/*.scss', ['sass']);
+    // gulp.watch('sass/*.scss', ['sass']);
+
+    gulp.watch(['sass/*.scss'], function(cb) {
+        gulp.task('sass')();
+        cb();
+    });
 });
 
 //    gulp.src('./**/*.scss', {base: './'})
